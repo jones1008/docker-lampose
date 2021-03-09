e43b789184d196b3f762410f08027f1cdec83eeb
jones1008 <jones@fluhrers.de>
2021-03-09 10:11:39 +0100

    fixed git submodule update error

--- _docker/web/scripts/git-submodule-update.sh
+++ _docker/web/scripts/git-submodule-update.sh
@@ -3,6 +3,6 @@
 if [ "$CONTAINS_GIT_SUBMODULES" != "false" ]; then
   if [ -f "./.gitmodules" ] || [ "$CONTAINS_GIT_SUBMODULES" = "true" ]; then
     echo "[INFO]: git-submodule-update.sh: trying to pull git submodule updates"
-    git submodule update --init --recursive
+    git submodule update --init --recursive || true
   fi
 fi
